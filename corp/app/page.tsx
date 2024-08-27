8d96b11ba341de9725a4ac23ebde0ec158de8c03
Bernard Asiedu Owusu-Dankwah <bennydan7.owusub243@gmail.com>
2024-08-27 15:05:48 +0000

    update

--- corp/app/page.tsx
+++ corp/app/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-   <div>Home Page</div>
+    <div>
+    <div>
+      <Link href="/performance">Performance</Link>
+      <Link href="/reliabilty">Reliability</Link>
+      <Link href="/scale">Scale</Link>
+    </div>
+        Home Page
+    </div>
   );
 }
